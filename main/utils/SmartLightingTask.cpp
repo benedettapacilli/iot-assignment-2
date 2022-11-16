b89539f99caba80cd4c0e3130df898596aac554d
Salvatore Antonio Addimando <salvaddimando@gmail.com>
2022-11-16 18:25:41 +0100

    Created SmartLightingTask (to be integrated with WaterLevelMonitoringTask)

---
+++ main/utils/SmartLightingTask.cpp
@@ -0,0 +1,54 @@
+#include "SmartLightingTask.h"
+
+SmartLightingTask::SmartLightingTask(int sensorPin, int lsPin, int pinLa)
+{
+    this->pirPin = sensorPin;
+    this->lsPin = lsPin;
+    this->led = Led(pinLa);
+}
+
+void SmartLightingTask::init(int period)
+{
+    Task::init(period);
+    this->state = IDLE;
+}
+
+void SmartLightingTask::tick()
+{   
+    //TODO: check if situation is ALARM and if so, set state to OFF
+
+    switch (this->state)
+    {
+    case IDLE:
+        int detected = analogRead(this->pirPin);
+
+        if (detected)
+        {
+            state = DETECTED;
+            this->T1offset = millis();
+        }
+        break;
+
+    case DETECTED:
+        int lsValue = analogRead(this->lsPin);
+        if (lsValue < THl)
+        {
+            this->led.on();
+        }
+        else
+        {
+            this->state = IDLE;
+        }
+
+        if (millis() - this->T1offset > T1)
+        {
+            this->state = IDLE;
+        }
+        break;
+
+    case OFF:
+        this->led.off();
+        //TODO: check if situation is NORMAL or PREALARM and if so, set state to IDLE
+        break;
+    }
+}
---
+++ main/utils/SmartLightingTask.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "Task.h"
+#include "Led.h"
+#define THl 500
+#define T1 1000
+
+class SmartLightingTask : public Task
+{
+private:
+    int pirPin;
+    int lsPin;
+    Led led;
+    int T1offset;
+
+public:
+    State state;
+    SmartLightingTask(int pirPin, int lsPin, int pinLa);
+    void init(int period);
+    void tick();
+};
+
+enum State
+{
+    IDLE,
+    DETECTED,
+    OFF
+};
