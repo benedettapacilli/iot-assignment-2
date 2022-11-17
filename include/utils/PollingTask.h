9a04e2d94aadf660fe5e1156f800e9b1415c8ccd
ValentinaPieri <valentina.pieri5@studio.unibo.it>
2022-11-17 18:12:43 +0100

    created file PollingTask.h to manage the polling of the water level

---
+++ include/utils/PollingTask.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "Task.h"
+#include "Arduino.h"
+#define PEnormal 2000
+#define PEprealarm 1000
+#define PEalarm 500
+
+class PollingTask : public Task
+{
+private:
+    int sonarTrigPin; // trig 9
+    int sonarEchoPin; // echo 8
+    int lastTimePolled;
+
+public:
+    PollingTask(int sonarPin1, int sonarPin2);
+    int pollWaterLevel();
+    void init(int period);
+    void tick();
+    int waterLevel;
+};
