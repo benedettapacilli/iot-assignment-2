115ac676e2b20e8a1d6577fbcd4b0f50f538553d
expl0r3rgu1 <salvaddimando@gmail.com>
2022-11-21 13:12:30 +0100

    Renamed main.ino to main.cpp and init SmartLightingTask

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+#include <Arduino.h>
+#include "../include/utils/Scheduler.h"
+#include "../lib/utils/Scheduler.cpp"
+#include "../include/utils/SmartLightingTask.h"
+
+#define PIR_PIN 3
+#define LS_PIN A0
+#define LA_PIN 2
+
+Scheduler scheduler;
+
+void setup()
+{
+    Serial.begin(9600);
+    scheduler.init(100);
+
+    Task *smartLightingTask = new SmartLightingTask(PIR_PIN, LS_PIN, LA_PIN);
+    smartLightingTask->init(100);
+
+    scheduler.addTask(smartLightingTask);
+}
+
+void loop()
+{
+    scheduler.schedule();
+}
