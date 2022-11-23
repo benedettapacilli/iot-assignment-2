b54bfabe344a94eca49538692f2c63341c7947e3
expl0r3rgu1 <salvaddimando@gmail.com>
2022-11-23 20:08:18 +0100

    Completed implementation of states except for valve opening

--- include/polling/PollingTask.h
+++ include/polling/PollingTask.h
@@ -1,21 +1,41 @@
 #pragma once
+#include "Arduino.h"
 #include "../utils/Task.h"
 #include "../utils/Sonar.h"
-#include "Arduino.h"
+#include "../utils/Led.h"
+#include "../utils/Potentiometer.h"
+#include "TimerOne.h"
+#include "LiquidCrystal_I2C.h"
+
 #define PEnormal 2000
 #define PEprealarm 1000
 #define PEalarm 500
+#define WL1 50
+#define WL2 100
+#define WLMAX 150
+#define BLINKING_INTERVAL 2000
+
+LiquidCrystal_I2C lcd;
 
 class PollingTask : public Task
 {
 private:
     Sonar sonar;
     int lastTimePolled;
+    int lastTimeBlinked;
     int currentPE;
+    Led lb;
+    Led lc;
+    Potentiometer pot;
+
+    void pollWaterLevel();
+    void blinkLc();
+    void showPreAlarmInfo();
+    void showAlarmInfo();
+    int getValveOpeningDegree();
 
 public:
-    PollingTask(int trigPin, int echoPin);
-    void pollWaterLevel();
+    PollingTask(int trigPin, int echoPin, int lbPin, int lcPin, int potPin);
     void init(int period);
     void tick();
     int waterLevel;
--- lib/polling/PollingTask.cpp
+++ lib/polling/PollingTask.cpp
@@ -1,31 +1,58 @@
 #include "../../include/polling/PollingTask.h"
 #include "../../include/utils/Situation.h"
 
-PollingTask::PollingTask(int trigPin, int echoPin)
+PollingTask::PollingTask(int trigPin, int echoPin, int lbPin, int lcPin, int potPin)
 {
     this->sonar = Sonar(trigPin, echoPin);
+    this->lb = Led(lbPin);
+    this->lc = Led(lcPin);
+    this->pot = Potentiometer(potPin);
+    lcd = LiquidCrystal_I2C(0x27, 20, 4);
 }
 
 void PollingTask::init(int period)
 {
     Task::init(period);
     this->pollWaterLevel();
+    this->lastTimeBlinked = millis();
+    lcd.init();
+    lcd.backlight();
 }
 
 void PollingTask::tick()
 {
+    if (this->waterLevel >= 0 && this->waterLevel <= WL1)
+    {
+        situation = NORMAL;
+    }
+    else if (this->waterLevel > WL1 && this->waterLevel <= WL2)
+    {
+        situation = PREALARM;
+    }
+    else if (this->waterLevel > WL2 && this->waterLevel <= WLMAX)
+    {
+        situation = ALARM;
+    }
+
     switch (situation)
     {
     case NORMAL:
         this->currentPE = PEnormal;
+        this->lb.on();
+        this->lc.off();
         break;
 
     case PREALARM:
         this->currentPE = PEprealarm;
+        this->blinkLc();
+        showPreAlarmInfo();
         break;
 
     case ALARM:
         this->currentPE = PEalarm;
+        this->lb.off();
+        this->lc.on();
+        showAlarmInfo();
         break;
     }
 
@@ -38,5 +65,43 @@ void PollingTask::tick()
 void PollingTask::pollWaterLevel()
 {
     this->lastTimePolled = millis();
-    this->waterLevel = this->sonar.getDistance();
+    int distance = this->sonar.getDistance();
+    this->waterLevel = (distance > WLMAX) ? WLMAX : distance;
+}
+
+void PollingTask::blinkLc()
+{
+    if (millis() - lastTimeBlinked >= BLINKING_INTERVAL)
+    {
+        this->lastTimeBlinked = millis();
+        this->lc.toggle();
+    }
+}
+
+void PollingTask::showPreAlarmInfo()
+{
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Pre-alarm");
+    lcd.setCursor(0, 1);
+    lcd.print("Water level: ");
+    lcd.print(this->waterLevel);
+}
+
+void PollingTask::showAlarmInfo()
+{
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Alarm");
+    lcd.setCursor(0, 1);
+    lcd.print("Water level: ");
+    lcd.print(this->waterLevel);
+    lcd.setCursor(0, 2);
+    lcd.print("Valve opening: ");
+    lcd.print(getValveOpeningDegree());
+}
+
+int PollingTask::getValveOpeningDegree()
+{
+    return map(this->pot.read(), 0, 1023, 0, 180);
 }
