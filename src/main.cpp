aa80998717c5529e942bd26fd92489ef1fab1689
benedettapacilli <benedetta.pacilli@studio.unibo.it>
2022-11-24 14:37:00 +0100

    Added polling task in main

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,16 @@
 #include "../lib/utils/Scheduler.h"
 #include "../lib/utils/Scheduler.cpp"
 #include "../lib/smart_lighting/SmartLightingTask.h"
+#include "../lib/polling/PollingTask.h"
 
 #define PIR_PIN 3
 #define LS_PIN A0
 #define LA_PIN 2
+#define LB_PIN 4
+#define LC_PIN 5
+#define SONAR_TRIG_PIN 9
+#define SONAR_ECHO_PIN 8
+#define POT_PIN A1
 
 Scheduler scheduler;
 
@@ -15,9 +21,13 @@ void setup()
     scheduler.init(100);
 
     Task *smartLightingTask = new SmartLightingTask(PIR_PIN, LS_PIN, LA_PIN);
+    Task *pollingTask = new PollingTask(SONAR_TRIG_PIN, SONAR_ECHO_PIN, LB_PIN, LC_PIN, POT_PIN);
+
     smartLightingTask->init(100);
+    pollingTask->init(100);
 
     scheduler.addTask(smartLightingTask);
+    scheduler.addTask(pollingTask);
 }
 
 void loop()
