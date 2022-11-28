70467dca990651aba3810ee240349e806446ceda
benedettapacilli <benedetta.pacilli@studio.unibo.it>
2022-11-28 14:55:00 +0100

    Modified serial output

--- lib/connector/ConnectorTask.cpp
+++ lib/connector/ConnectorTask.cpp
@@ -11,7 +11,10 @@ void ConnectorTask::init(int period)
 
 void ConnectorTask::tick()
 {
+    Serial.print("s ");
     Serial.println(situation);
+    Serial.print("b ");
     Serial.println(bridgeLightStatus);
+    Serial.print("w ");
     Serial.println(waterLevel);
 }
